41bb6f34ea6962bf033ee7b877dd182a980d8dda
Kazuhiro Kojio <kazuhiro.kojio@gmail.com>
2020-12-15 20:41:13 -0300

    test: validate emails with empty domain

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -42,4 +42,10 @@ describe('Email validation', () => {
 
     expect(Email.validate(email)).toBeFalsy()
   })
+
+  test('should not accept empty domain', () => {
+    const email: string = 'any@'
+
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
