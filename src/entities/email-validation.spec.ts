1e8d32c9997ba45bf079d7d064298918d7cfcdf6
Kazuhiro Kojio <kazuhiro.kojio@gmail.com>
2020-12-15 20:20:24 -0300

    test: validate emails with more than 320 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -24,4 +24,10 @@ describe('Email validation', () => {
 
     expect(Email.validate(email)).toBeFalsy()
   })
+
+  test('should not accept strings larger than 320 chars', () => {
+    const email: string = 'l'.repeat(64) + '@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
+
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
